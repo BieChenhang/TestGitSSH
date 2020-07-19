c83b0f3fee7807ddb81d36b6df020bfe955fffc3
tom_glb <goodMorning_glb@atguigu.com>
2020-07-19 17:32:49 +0800

    1test gitssh

---
+++ src/main/java/com/bch/test/TestGitSSH.java
@@ -0,0 +1,21 @@
+package com.bch.test;
+
+/** 
+ * @description:
+ * @author BieChenhang 
+ * @date 2020年7月19日 
+*/
+public class TestGitSSH {
+
+	/**   
+	 * @Description: TODO 
+	 * @param @param args
+	 * @return void
+	 * @author BieChenhang
+	 * @date 2020年7月19日  
+	*/
+	public static void main(String[] args) {
+		System.out.println("test git ssh");
+	}
+
+}
